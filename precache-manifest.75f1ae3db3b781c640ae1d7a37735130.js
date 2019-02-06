2fab30b6496fe0a7c3a2c953af0d82a7b1b985a3
zosime <zosime@naver.com>
2019-02-06 14:15:53 +0900

    Updates

---
+++ precache-manifest.75f1ae3db3b781c640ae1d7a37735130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "9bc5f14792d78226596b",
+    "url": "/movie-app/static/js/main.9bc5f147.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie-app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "9bc5f14792d78226596b",
+    "url": "/movie-app/static/css/main.7abfd5d6.chunk.css"
+  },
+  {
+    "revision": "3ac88a6e709422a7e56e41d9e0d3f51b",
+    "url": "/movie-app/index.html"
+  }
+];
